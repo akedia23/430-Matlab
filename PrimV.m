13fc1e2dcfecbf1a811cb75274dab378ab960103
Joe <joebanko15@gmail.com>
2020-03-11 10:50:36 -0700

    Move PrimV to an enum

--- PrimV.m
+++ PrimV.m
@@ -1,17 +1,5 @@
 classdef PrimV < Value
-    
-    properties
-        f
-    end
-    
-    methods
-        function obj = PrimV(f)
-            obj.f = f;
-        end
-        
-        function outputF = getF(obj)
-            outputF = obj.f;
-        end
-    end
+   enumeration
+      Add, Sub, Mult, Div, Leq, Eq
+   end
 end
-
