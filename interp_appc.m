b20ac080d8cd375935706c7daf993201e1365fe8
Akash <kedia23akash@gmail.com>
2020-03-11 11:32:42 -0700

    spacing

--- interp_appc.m
+++ interp_appc.m
@@ -1,13 +1,13 @@
 function val = interp_appc(expr, env)
-            funval = interpretor(expr, env);
-            argvals = [];
-            for arg = expr.args
-                argvals.append(interpretor(arg, env));
-            end
-            if isa(funval, 'CloV')
-                new-env = bind(funval.params, argvals).append(env);
-                val = interpretor(funval.body, new-env);
-            elseif isa(funval, 'PrimV')
-                val = funval.f(argvals);
-            end
-        end
+    funval = interpretor(expr, env);
+    argvals = [];
+    for arg = expr.args
+        argvals.append(interpretor(arg, env));
+    end
+    if isa(funval, 'CloV')
+        new-env = bind(funval.params, argvals).append(env);
+        val = interpretor(funval.body, new-env);
+    elseif isa(funval, 'PrimV')
+        val = funval.f(argvals);
+    end
+end
