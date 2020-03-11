818130823f972724ae334ff7de669b10b8c04a19
matthewlewis112 <matthewlewis112@gmail.com>
2020-03-11 01:02:09 -0700

    Example of interpretor cases

--- interpretor.m
+++ interpretor.m
@@ -1,18 +1,28 @@
 %ExprC definition%
-NumC = struct(n,[]);
-IdC = struct(s,[]);
-StrgC = struct(str,[]);
-LamC = struct(params,[],body,[]);
-AppC = struct(fun,[],args,[]);
-IfC = struct(tst,[],thn,[],els,[]);
+% NumC = struct(n,[]);
+% IdC = struct(s,[]);
+% StrgC = struct(str,[]);
+% LamC = struct(params,[],body,[]);
+% AppC = struct(fun,[],args,[]);
+% IfC = struct(tst,[],thn,[],els,[]);
 %ExprC = struct(numC,NumC,idC,IdC,strgC,StrgC,lamC,LamC,appC,AppC,ifC,IfC);%
 
 %Value definition%
-NumV = struct(n,[]);
-BoolV = struct(b,[]);
-StrgV = struct(str,[]);
-CloV = struct(params,[],body,[],env,[]);
-PrimV = struct(f,[]);
+% NumV = struct(n,[]);
+% BoolV = struct(b,[]);
+% StrgV = struct(str,[]);
+% CloV = struct(params,[],body,[],env,[]);
 
-    
+
+%Convert an ExprC into a Value%
+function val = interpretor(expr)
+    % PrimV = struct(f,[]);
+    import NumC
+    import StrgC
+    if isa(expr, 'NumC')
+        val = expr.num;
+    elseif isa(expr, 'StrgC')
+        val = expr.str;
+    end
+end
 
