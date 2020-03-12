443854383d1446dda67d9fe0bfdb7cc2d4c1ebcb
matthewlewis112 <matthewlewis112@gmail.com>
2020-03-12 07:40:31 -0700

    Added some more code for vars interp

--- interp_vars.m
+++ interp_vars.m
@@ -1,10 +1,11 @@
-function [expr] = interp_vars(expr,vars)
+function expr = interp_vars(expr,vars,top_env)
     %INTERP_VARS Substitute var values into vars body
     %   Detailed explanation goes here
     env = [];
-    while not(ISEMPTY(vars))
+    for i = 1:length(vars)
         % append var bindings to env %
+        append(env, Binding(vars(i, 1), vars(i, 2)));
     end
-    interp(expr, env)
+    expr = interpretor(expr, horz_cat(top_env, env));
 end
 
