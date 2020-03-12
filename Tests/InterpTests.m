20b6e24aabca5f70b57ac7a617fe3e06add3f254
Joe <joebanko15@gmail.com>
2020-03-11 22:32:07 -0700

    Add more test cases for interp

--- Tests/InterpTests.m
+++ Tests/InterpTests.m
@@ -19,6 +19,51 @@ classdef InterpTests < matlab.unittest.TestCase
             actSolution = interpretor(IdC("not-primop"), env);
             testCase.verifyEqual(actSolution.str, "value");
         end
+        function testMultInterp(testCase)
+            exp = AppC(IdC("*"), [NumC(5), NumC(7)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.num, 35);
+        end
+        function testAddInterp(testCase)
+            exp = AppC(IdC("+"), [NumC(5), NumC(7)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.num, 12);
+        end
+        function testDivInterp(testCase)
+            exp = AppC(IdC("/"), [NumC(7), NumC(5)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.num, 1.4);
+        end
+        function testSubInterp(testCase)
+            exp = AppC(IdC("-"), [NumC(5), NumC(7)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.num, -2);
+        end
+        function testLeqTrueInterp(testCase)
+            exp = AppC(IdC("<="), [NumC(5), NumC(7)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.bool, true);
+        end
+        function testLeqTrue2Interp(testCase)
+            exp = AppC(IdC("<="), [NumC(5), NumC(5)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.bool, true);
+        end
+        function testLeqFalseInterp(testCase)
+            exp = AppC(IdC("<="), [NumC(7), NumC(5)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.bool, false);
+        end
+        function testEqFalseInterp(testCase)
+            exp = AppC(IdC("equal?"), [NumC(7), NumC(5)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.bool, false);
+        end
+        function testEqTrueInterp(testCase)
+            exp = AppC(IdC("equal?"), [NumC(5), NumC(5)]);
+            actSolution = interpretor(exp, topenv());
+            testCase.verifyEqual(actSolution.bool, true);
+        end
     end
 
 end
